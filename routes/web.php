4e513adbbc1f1f92607f951affd6cc24bdc6797c
aziz <azizfhahaha@gmail.com>
2024-08-22 23:37:53 +0700

    feat: Add new admin layout and create migration for admin table

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+// route portfolio
 Route::get('/', function () {
     return view('pages.index');
 });
-
 
 Route::get('/about', function () {
     return view('pages.about');
@@ -22,4 +23,10 @@ Route::get('/portofolio', function () {
 
 Route::get('/contact', function (){
     return view('pages.contact');
+});
+
+
+// route admin
+Route::get('/admin', function (){
+    return view('admin.index');
 });
